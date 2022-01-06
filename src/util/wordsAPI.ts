3e0577ac20f1d930db38458af5718813b88fd246
nraff <nraff510@gmail.com>
2022-01-06 12:36:22 -0800

    fix string filter issues

--- src/util/wordsAPI.ts
+++ src/util/wordsAPI.ts
@@ -13,11 +13,12 @@ const auth = {
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-function setRegex(reqLetter, searchLetters) {
-  const excludeLetters = alphabet.filter(char => {
+function setRegex(reqLetter: string, searchLetters: string) {
+  const alphas = alphabet.split('')
+  const excludeLetters = alphas.filter((char: string) => {
     return (char !== reqLetter && !searchLetters.includes(char))
   })
-  
+
 }
 
 export {}
