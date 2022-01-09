77be385d28c82e74a25949009ae17d137668359b
nraff <nraff510@gmail.com>
2022-01-08 19:21:38 -0800

    add image

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,11 +5,13 @@ import Letters from './Letters';
 import HintButton from './HintButton';
 import Words from './Words';
 import { SBContext } from '..';
+import Credits from './Credits';
 
 function App() {
   const state = useContext(SBContext)
   return (
     <div className="App">
+      <Credits />
       <h1>Spelling Bee Solver</h1>
       <Letters numLetters={7} />
       <Words />
---
+++ src/components/Credits.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../assets/sb-solver-logo.png"
+
+function Credits() {
+  return (
+    <section className="credits-area">
+      <div className="about-logo">
+        <img src={logo} alt="logo" />
+        <h3>About</h3>
+      </div>
+    </section>
+  )
+}
+
+export default Credits;
