1b56067eebcc84a08316ba175e0b603a019e385d
nraff <nraff510@gmail.com>
2022-01-09 21:25:30 -0800

    complete tooltip v1

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -9,6 +9,7 @@ function Tooltip({wordObj}:props) {
   const {details, word} = wordObj
   return (
     <div className="tooltip">
+      <div className="arrow-up"></div>
       <h2>{word}</h2>
       <ul className="def-list">
         {details.definitions.map((def: any, idx: number) => <TooltipItem key={idx} definition={def} />)}
