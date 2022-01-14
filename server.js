6a339649a0f99f1ead82ff902241a4d73b70e624
nraff <nraff510@gmail.com>
2022-01-14 15:21:07 -0800

    update path to index.html

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ const port = process.env.PORT || 8080
 const path = require('path')
 const bodyParser = require('body-parser');
 const getWordObjects = require('./api/wordsUtil')
+const publicPath = path.join(__dirname, '..', 'public');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'))
+  // app.use(express.static('frontend/build'))
+  app.use(express.static(publicPath))
   // app.use(express.static(path.join(__dirname, 'build')))
   app.get('/', (req, res) =>  {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    // res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     // res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(publicPath, 'index.html'))
   })
 }
+
+// "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix frontend && npm run build --prefix frontend"
 
 app.use(bodyParser.urlencoded({
   extended: false
