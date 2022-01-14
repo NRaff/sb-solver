6de8546291fc302bc078f07bf7edef3e5489a610
nraff <nraff510@gmail.com>
2022-01-14 14:50:21 -0800

    add path requirement for server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080
+const path = require('path')
 const bodyParser = require('body-parser');
 const getWordObjects = require('./api/wordsUtil')
 
