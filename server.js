f1099ec788677f6a33bc53f53435cdb2e244d829
nraff <nraff510@gmail.com>
2022-01-14 15:09:12 -0800

    add post build script

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const bodyParser = require('body-parser');
 const getWordObjects = require('./api/wordsUtil')
 
 if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static('frontend/build'))
-  app.use(express.static(path.join(__dirname, 'build')))
+  app.use(express.static('frontend/build'))
+  // app.use(express.static(path.join(__dirname, 'build')))
   app.get('/', (req, res) =>  {
-    // res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    // res.sendFile(path.join(__dirname, 'build', 'index.html'))
   })
 }
 
