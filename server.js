8a91329bc3ae37c6b55e46c2ccf87b46af192f25
nraff <nraff510@gmail.com>
2022-01-15 17:00:12 -0800

    adjust to mirror past projects

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ const getWordObjects = require('./api/wordsUtil')
 const publicPath = path.join(__dirname, 'frontend/public');
 
 if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static('frontend/build'))
-  // app.use(express.static(publicPath))
-  app.use(express.static(path.join(__dirname, 'build')))
-  app.get('/', (req, res) =>  {
-    // res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    // res.sendFile(path.join(__dirname, 'build', 'index.html'))
-    res.sendFile(path.join(publicPath, 'index.html'))
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
+  // app.use(express.static('frontend/build'))
+  // // app.use(express.static(publicPath))
+  // app.use(express.static(path.join(__dirname, 'build')))
+  // app.get('/', (req, res) =>  {
+  //   // res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  //   // res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  //   res.sendFile(path.join(publicPath, 'index.html'))
+  // })
 }
 
 // "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix frontend && npm run build --prefix frontend"
