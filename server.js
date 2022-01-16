e685c40a9bc50a752a2e467ec81e86b242750772
nraff <nraff510@gmail.com>
2022-01-15 17:12:25 -0800

    manual build

--- server.js
+++ server.js
@@ -11,14 +11,6 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
-  // app.use(express.static('frontend/build'))
-  // // app.use(express.static(publicPath))
-  // app.use(express.static(path.join(__dirname, 'build')))
-  // app.get('/', (req, res) =>  {
-  //   // res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  //   // res.sendFile(path.join(__dirname, 'build', 'index.html'))
-  //   res.sendFile(path.join(publicPath, 'index.html'))
-  // })
 }
 
 // "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix frontend && npm run build --prefix frontend"
