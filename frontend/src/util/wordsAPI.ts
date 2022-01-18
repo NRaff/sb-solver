595c85c489015d9eb877a68f3dc6deefc23d90bf
nraff <nraff510@gmail.com>
2022-01-18 14:32:50 -0800

    remove unnecessary flags

--- frontend/src/util/wordsAPI.ts
+++ frontend/src/util/wordsAPI.ts
@@ -1,6 +1,5 @@
 import axios from "axios"
 
-// ! Request effectively times out on mobile
 export function requestWords(reqLetter: string, searchLetters: string) {
   const letters = (reqLetter + searchLetters).split('')
   const params = {
