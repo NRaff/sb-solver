edaf0917c0ce16900144018c4bb28466dfe7d7d7
nraff <nraff510@gmail.com>
2022-01-20 22:25:19 -0800

    fix header spacing

--- frontend/src/components/OneWord.tsx
+++ frontend/src/components/OneWord.tsx
@@ -5,9 +5,24 @@ interface props {
   isPanagram: boolean
 }
 
+function scrollTo({id}: any) {
+  console.log(id)
+  const el = document.getElementById(id)
+  console.log(el)
+  el?.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+    inline: "center"
+  })
+}
+
 function Word({wordObj, isPanagram}: props) {
   return (
-    <section className={`word${isPanagram ? ' panagram' : ''}`}>
+    <section 
+      className={`word${isPanagram ? ' panagram' : ''}`} 
+      id={wordObj.word}
+      onClick={({target}) => scrollTo(target)}
+    >
       <p>{wordObj.word}</p>
       <Tooltip wordObj={wordObj} />
     </section>
