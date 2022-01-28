a5b4a2280cf3530bdfb2a59334b86b344b7e5665
nraff <nraff510@gmail.com>
2022-01-28 10:09:53 -0800

    remove unnecessary dom query

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -15,17 +15,14 @@ declare global {
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("dom content loaded")
-  ReactDOM.render(
-    <React.StrictMode>
-      <SBContext.Provider value={sbDetails}>
-        <App />
-      </SBContext.Provider>
-    </React.StrictMode>
-    ,
-    document.getElementById('root')
-  );
-})
-
 export const SBContext = React.createContext(sbDetails)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SBContext.Provider value={sbDetails}>
+      <App />
+    </SBContext.Provider>
+  </React.StrictMode>
+  ,
+  document.getElementById('root')
+);
